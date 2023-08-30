d22bcccbc42b6a9ae884d0ec2d805b5895dd21cf
AndreaChiari <chiariandrea94@gmail.com>
2023-08-30 14:49:14 +0200

    add home page link to logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,12 @@
 <script></script>
 <template>
-   <header class="flex justify-between items-center container mx-auto" >
+   <header>
+    <router-link :to="{name: 'home'}" class="flex justify-between items-center container mx-auto">
         <img class="h-full w-48" src="../assets/img/MOTORK-logo-2048x463.png" alt="MOTORK-logo-2048x463.png">
         <div class="p-1">
             <h4 class="text-center font-sans font-semibold">MOTORK <br> CONCESSIONARY</h4>
         </div>
+    </router-link>
    </header>
 </template>
 
