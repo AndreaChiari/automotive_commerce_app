f69b50e87a8cff4a92f7c9f4e7092c2c57aecdb1
AndreaChiari <chiariandrea94@gmail.com>
2023-08-30 15:15:36 +0200

    set up html for filter feature

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -20,6 +20,10 @@ export default {
 </script>
 <template>
     <main class="container mx-auto row flex flex-wrap justify-center items-center">
+        <div class="flex justify-center filter-container">
+            <input id="filterinput" type="text" placeholder="Search for a car" class="mr-4">
+            <button id="filterbutton"> GO</button>
+        </div>     
         <ProductCard v-for="car in cars" :key="car.id" :make="car.make" :model="car.model" :price="car.price" :id="car.id"></ProductCard>
     </main>
 </template>
@@ -28,6 +32,20 @@ export default {
 main{
     gap: 25px;
     overflow-y: hidden;
-    min-height: calc(100vh - 260px)
+    min-height: calc(100vh - 260px);
+    .filter-container{
+        padding: 0;
+    }
+    input{
+        padding-left: 2px;
+        border: 1px solid #F90032;
+    }
+    button{
+      color: white;
+      background: #F90032;
+      border: 1px solid black;
+      padding: 3px 6px;
+    }
+   
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,10 @@ import DetailPage from "../pages/DetailPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-routes:[
-  { path: "/", name: "home", component: HomePage },
-  { path: "/car/:id", name: "DetailPage", component: DetailPage }
-]
+  routes:[
+    { path: "/", name: "home", component: HomePage },
+    { path: "/car/:id", name: "DetailPage", component: DetailPage }
+  ]
 })
 
 export {router};
